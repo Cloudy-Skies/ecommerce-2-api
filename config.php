f67298ea9c4961b0415a299ae36b9d5ee412ebff
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-24 10:06:51 +0000

    edited credentials with appropriate values

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'class_contact_managment');
+define('DB_SERVER', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USERNAME', 'b2a21de5a2ce10');
+define('DB_PASSWORD', '1b17406f ');
+define('DB_NAME', 'heroku_408a343ea3e0bac');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
